85fbd91a5923809a813e8babf978cdd4c4aa94c9
Prissilla (Laila Sandra) <a.g.marosi@bluewin.ch>
2026-03-31 22:00:49 +0200

    Bro, wtf is this spec 😭 i don't understand anything

--- src/protocols/gptp/md_sync_receive_sm.rs
+++ src/protocols/gptp/md_sync_receive_sm.rs
@@ -1,6 +1,28 @@
-enum MDSyncReceiveSM
+pub struct MDSyncReceiveSM
 {
-  Discard,
-  WaitingForFollowUp,
-  WaitingForSync
+  state: State,
+
+  follow_up_receipt_timeout_time: (),
+  received_sync: bool,
+  received_follow_up: bool,
+  received_sync_pointer: (),
+  received_follow_up_pointer: (),
+  tx_md_sync_received_pointer_mdsr: (),
+  upstream_sync_interval: (),
+  temp: i16,
+}
+
+enum State { Discard, WaitingForFollowUp, WaitingForSync }
+
+impl MDSyncReceiveSM
+{
+  pub fn set_md_sync_receive_mdsr(&mut self) -> Self
+  {
+    todo!()
+  }
+
+  pub fn tx_md_sync_receive(&mut self, tx_md_sync_receive_pointer_mdsr: ())
+  {
+    todo!()
+  }
 }
